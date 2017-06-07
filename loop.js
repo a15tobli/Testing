fcfc4b409e06ba520085023d452f5a60b34be653
Unknown <tobias.l.95@hotmail.com>
2017-06-07 21:39:46 +0200

    Added bouncer

---
+++ loop.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("testCanvas");
+var ctx = canvas.getContext('2d');
+function updateGame(){
+  ctx.fillStyle="#385";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
