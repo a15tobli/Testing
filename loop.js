f50b3cd9b1f38584593b211040bf83ab0dcf0713
Unknown <tobias.l.95@hotmail.com>
2017-06-07 21:41:45 +0200

    Added bouncing rectangle

--- loop.js
+++ loop.js
@@ -1,6 +1,33 @@
 var canvas = document.getElementById("testCanvas");
 var ctx = canvas.getContext('2d');
+var dx = -2;
+var dy = -3;
+function drawCanvas(){
+  updateGame();
+}
 function updateGame(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle="#385";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  drawRectangle();
+  console.log("Test");
+  requestAnimationFrame(updateGame);
+}
+
+var y = canvas.height-150;
+var x = canvas.width-150;
+function drawRectangle(){
+  ctx.beginPath();
+  ctx.rect(x, y, 150, 150);
+  ctx.stroke();
+  ctx.closePath();
+  if(x+150 + dx > canvas.width || x + dx < 0) {
+    dx = -dx;
+}
+
+if(y+150 + dy > canvas.height || y + dy < 0) {
+    dy = -dy;
+}
+x += dx;
+y += dy;
 }
